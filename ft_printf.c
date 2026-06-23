3e64810cd3765c2121c756f372e534d265721f68
ANZ01 <danielsmduarte@gmail.com>
2026-06-23 15:22:06 +0100

    STARTING FT_PRINTF; STOPPED TO UNDERSTAND VARGS

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,33 @@
 /*   By: dmaurici <dmaurici@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 12:52:26 by dmaurici          #+#    #+#             */
-/*   Updated: 2026/06/23 12:58:23 by dmaurici         ###   ########.fr       */
+/*   Updated: 2026/06/23 15:06:14 by dmaurici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
+#include <stdarg.h>
 
-int ft_printf(const char *format, ...);
+int	conversion_reader(char symbol)
+{
+	if (symbol == 'c')
+		ft_putchar()
+}
+int ft_printf(const char *format, ...)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+		count += conversion_reader(*format + 1);
+			format++;
+		}	
+		else
+			count += ft_putchar(*format);	
+		format++;
+	}
+	return (count);
+}
